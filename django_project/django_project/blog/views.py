426cef08347c09af4a4694cbd48f6d02ea44f996
ekkamel <ekkamel@gmail.com>
2023-02-04 21:46:23 +0200

    first commit

---
+++ django_project/django_project/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS', 
+        'title': 'Blog Post 1', 
+        'content': 'First post content', 
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe', 
+        'title': 'Blog Post 2', 
+        'content': 'Second post content', 
+        'date_posted': 'August 28, 2018'
+    }     
+]
+
+
+# Create your views here.
+def home(request):
+    context = {
+        'posten': posts      #name of the dictionary
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})    #title is a title
